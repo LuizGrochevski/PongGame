e2fc5fec979076150cb777a758fb4ffca20cb0d0
LuizGrochevski <luizfelipegrochevski@gmail.com>
2023-12-08 16:08:53 -0300

    First commit

---
+++ src/Enemy.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Enemy {
+
+	public boolean right, left;
+	
+	public double x, y;
+	public int width, height;
+
+	public Enemy(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 5;
+	}
+
+	public void tick() {
+		x += (Game.ball.x - (x + 3)) - 0.07;
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.red);
+		g.fillRect((int) x, (int) y, width, height);
+	}
+
+}
